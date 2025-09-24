ccde19f0ebecea49c2beccd679f5ac47ef50835b
Anna Wallström <anoli89@hotmail.com>
2025-09-24 14:56:34 +0200

    Initial typescript setup

--- portfolio-web-app/src/App.tsx
+++ portfolio-web-app/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>TEST</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
